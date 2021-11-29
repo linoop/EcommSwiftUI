19671ba8dd86dcaa1e190773f49f1628671b093a
Linoop K K <linoop@gmail.com>
2021-11-29 23:10:53 +0530

    Initial Commit

---
+++ EcommApp/EcommAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommAppApp.swift
+//  EcommApp
+//
+//  Created by Linoop K K on 29/11/21.
+//
+
+import SwiftUI
+
+@main
+struct EcommAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
